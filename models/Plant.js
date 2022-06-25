c4024798d85af67d2f45d9c33dc30b01588eb991
avandefeniks <angel.vandefeniks@gmail.com>
2022-06-25 10:36:40 -0700

    created models and associations. completed connection. completed schema.sql

---
+++ models/Plant.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Plant extends Model {}
+
+Plant.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sun_intake: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        water_intake: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        soild_type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'plant'
+    }
+);
