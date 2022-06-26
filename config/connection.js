bdc963b5a8bd0a90ffd39c2fb516085ea6295532
Peter Tran <pettran866@gmail.com>
2022-06-25 19:28:50 -0700

    added checkPassword to User model, fixed assocations

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
 ? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
 });
+
+module.exports = sequelize;
