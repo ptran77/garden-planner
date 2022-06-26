fbb3cf914eb9f57122b178cbd77eb304c276b847
analizajb <96861913+analizajb@users.noreply.github.com>
2022-06-26 16:35:41 -0700

    updating pull from develop

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const userRoutes = require('./user-routes.js');
-
-router.use('/users', userRoutes);
-=======
 const userRoutes = require('./user-routes');
 const gardenRoutes = require('./garden-routes');
 const plantRoutes = require('./plant-routes');
@@ -11,6 +6,5 @@ const plantRoutes = require('./plant-routes');
 router.use('/users', userRoutes);
 router.use('/gardens', gardenRoutes);
 router.use('/plants', plantRoutes)
->>>>>>> 971aad30ef77d11d827a3a962ba171d02f5ded99
 
 module.exports = router;
