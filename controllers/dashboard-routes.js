e1ee0f3cf218b27a87c4a19c3e0a378deac1f0d7
Peter Tran <pettran866@gmail.com>
2022-06-28 13:39:57 -0700

    dashboard main page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const { Garden, Plant } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    res.render('dashboard')
+// /dashboard routes
+
+// default dashboard page
+router.get('/', withAuth, (req, res) => {
+    Garden.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'garden_name', 'created_at']
+    })
+        .then(dbGardenData => {
+            const gardens = dbGardenData.map(garden => garden.get({ plain: true }));
+            res.render('dashboard', { gardens, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
+
+
 
 module.exports = router;
