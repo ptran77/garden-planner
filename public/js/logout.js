9301aa93c2ccb6792842121ae0d6830a04242648
Peter Tran <pettran866@gmail.com>
2022-06-30 13:09:52 -0700

    added logout.js

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+// Makes post request to logout and stop the session
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    document.location.replace('/login');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
