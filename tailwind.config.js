f267cce4f694646948c6d14146cfde91b6adb1ba
analizajb <96861913+analizajb@users.noreply.github.com>
2022-07-07 15:42:26 -0700

    adding content to tailwindconfig/commenting media queries for future ref in css / added uitility classes in main.hbs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "../main.handlebars",
+    "./zoneSearch.handlebars",
+    "./single-plant",
+    "./signup",
+    "./plants",
+    "./login",
+    "./homepage",
+    "./editGarden",
+    "./dashboard",
+    "./createGarden",
+    "./addToGarden",
+    "./add-plant"
+  ],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      'blue': '#1fb6ff',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      blue: "#1fb6ff",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
     },
     extend: {
       spacing: {
-        '5px': '5px',
-      }
+        "5px": "5px",
+      },
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-      'display': ['Pacifico'],
-      'body': ['Pangolin']
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      display: ["Pacifico"],
+      body: ["Pangolin"],
     },
   },
-}
+};
