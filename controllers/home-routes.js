458722945dc68855dcd79bed2230c3114f6908a3
analizajb <96861913+analizajb@users.noreply.github.com>
2022-07-07 14:17:51 -0700

    taking out community home-routes/community pages/adding media queries

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -25,68 +25,6 @@ router.get("/home", (req, res) => {
   res.render("homepage")
 });
 
-router.get("/community", (req, res) => {
-  console.log('======================');
-  Garden.findAll({
-    attributes: [
-      'id', 'garden_name', 'user_id'
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbGardenData => {
-      const gardens = dbGardenData.map(garden => garden.get({ plain: true }));
-
-      // Render the community page
-      res.render('community', {
-        gardens,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get("/community/garden:id", (req, res) => {
-  Garden.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: [
-      'id', 'garden_name', 'user_id'
-    ],
-    include: [
-      {
-      model: User,
-      attributes: ['username']
-      }
-    ]
-  })
-    .then(dbGardenData => {
-      if (!dbGardenData) {
-        res.status(404).json({ message: 'No gardens found with this id' });
-        return;
-      }
-
-      const garden = dbGardenData.get({ plain: true });
-
-      // Render single garden page
-      res.render('single-garden', {
-        garden,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
 
 router.get("/zone", (req, res) => {
   res.render("zoneSearch");
